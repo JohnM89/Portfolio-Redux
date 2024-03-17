991cdd05cadf259d017e64b263fb20ed153b81a8
JohnM89 <mjammer2@gmail.com>
2024-03-17 08:44:35 -0400

    template ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom';
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.scss'
 import AboutMe from './components/AboutMe/aboutme';
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom'
+// import reportWebVitals from './reportWebVitals'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    ,
   </React.StrictMode>,
+  document.getElementById('root')
 )
+
+
+// reportWebVitals()
