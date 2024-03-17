b7119230b5723b369022d2f77567e10ae6317066
JohnM89 <mjammer2@gmail.com>
2024-03-17 12:34:59 -0400

    project array template set up

--- src/components/Portfolio/portfolio.jsx
+++ src/components/Portfolio/portfolio.jsx
@@ -1,18 +1,20 @@
 // import { Routes, Route } from 'react-router-dom';
 import Project from '../Project/project';
+import React from 'react';
 
 const projects = [
   // Array of project data
 ];
 
-function Portfolio() {
+const Portfolio = () => {
   return (
     <div>
+      {/* Removed mouse animation related code */}
       {projects.map((project, index) => (
         <Project key={index} {...project} />
       ))}
     </div>
   );
-}
+};
 
 export default Portfolio;
