0c0c6e5d8fd6d69aab9dd8d7b7bed7664d8519f6
JohnM89 <mjammer2@gmail.com>
2024-03-17 12:58:34 -0400

    added nav bar styling

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -1,13 +1,34 @@
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import './navbar.scss'; // Import your SCSS file for styling
 
 function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav>
-      <NavLink to="/about">About Me</NavLink>
-      <NavLink to="/portfolio">Portfolio</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
-      <NavLink to="/resume">Resume</NavLink>
-    </nav>
+    <div className={`navigation-container ${isOpen ? 'open' : ''}`}>
+      {/* <button className="toggle-button" onClick={toggleModal}>
+        ☰
+      </button> */}
+      <nav className="navigation">
+        <NavLink to="/about" onClick={toggleModal}>
+          About Me
+        </NavLink>
+        <NavLink to="/portfolio" onClick={toggleModal}>
+          Portfolio
+        </NavLink>
+        <NavLink to="/contact" onClick={toggleModal}>
+          Contact
+        </NavLink>
+        <NavLink to="/resume" onClick={toggleModal}>
+          Resume
+        </NavLink>
+      </nav>
+    </div>
   );
 }
 
--- src/components/Portfolio/portfolio.jsx
+++ src/components/Portfolio/portfolio.jsx
@@ -1,17 +1,20 @@
-// import { Routes, Route } from 'react-router-dom';
-import Project from '../Project/project';
 // import React from 'react';
-
-const projects = [
-  // Array of project data
-];
-
+import Project from '../Project/project';
+import projects from '../../data/projects';
 const Portfolio = () => {
   return (
     <div>
-      {/* Removed mouse animation related code */}
-      {projects.map((project, index) => (
-        <Project key={index} {...project} />
+      {projects.map((project) => (
+        <div key={project.id}>
+          {project.liveLink ? (
+            <a href={project.liveLink} target="_blank" rel="noopener noreferrer">
+              <img src={project.imageUrl} alt={project.title} />
+            </a>
+          ) : (
+            <img src={project.imageUrl} alt={project.title} />
+          )}
+          <Project {...project} />
+        </div>
       ))}
     </div>
   );
