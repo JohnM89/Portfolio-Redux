7dbfc645ef862f6df0811429fd6eb72ca621cdc8
JohnM89 <mjammer2@gmail.com>
2024-03-17 10:08:35 -0400

    nested projects in portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,11 @@
-// import { useState } from 'react'
-import { Route, Routes } from 'react-router-dom';
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.scss'
+import { Routes, Route } from 'react-router-dom';
+import './App.scss';
 import AboutMe from './components/AboutMe/aboutme';
-
-import Contact from './components/Contact/contact';
-import Project from './components/Project/project';
 import Header from './components/Header/header';
 import Footer from './components/Footer/footer';
 import NavBar from './components/NavBar/navbar';
 import Portfolio from './components/Portfolio/portfolio';
-
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
+import Contact from './components/Contact/contact';
 
 function App() {
   return (
@@ -53,13 +18,10 @@ function App() {
         <Routes>
           <Route path="/" element={<AboutMe />} />
           <Route path="/about" element={<AboutMe />} />
+          <Route path="/portfolio/*" element={<Portfolio />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/projects" element={<Project />} />
-          <Route path="/portfolio" element={<Portfolio />} />
         </Routes>
       </main>
-
-      
 
       <Footer />
     </div>
@@ -67,4 +29,3 @@ function App() {
 }
 
 export default App;
-
--- src/components/Portfolio/portfolio.jsx
+++ src/components/Portfolio/portfolio.jsx
@@ -1,10 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Project from '../Project/project';
+
 function Portfolio() {
   return (
     <div>
       <h2>Portfolio</h2>
       <p>Portfolio section.</p>
+       <Routes>
+        <Route path="/projects" element={<Project />} />
+      </Routes>
     </div>
   );
 }
+
 
 export default Portfolio;
