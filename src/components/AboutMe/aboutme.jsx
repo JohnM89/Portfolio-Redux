6199388a3eb0826dfb2f2ee9753693c5da919c6c
JohnM89 <mjammer2@gmail.com>
2024-03-17 10:51:07 -0400

    added a .cubespinner to about me page

--- src/components/AboutMe/aboutme.jsx
+++ src/components/AboutMe/aboutme.jsx
@@ -5,26 +5,41 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faAngular, faHtml5, faCss3, faReact, faJsSquare, faGitAlt } from '@fortawesome/free-brands-svg-icons';
 import './AboutMe.css';
 
-library.add(fas, fab); // This adds all solid and brand icons to the library
+library.add(fas, fab); 
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; 
+  }
+  return array;
+}
 
 const iconColors = {
-    faAngular: "#DD0031",
-    faHtml5: "#F06529",
-    faCss3: "#28A4D9",
-    faReact: "#5ED4F4",
-    faJsSquare: "#EFD81D",
-    faGitAlt: "#EC4D28",
+  faAngular: "#DD0031",
+  faHtml5: "#F06529",
+  faCss3: "#28A4D9",
+  faReact: "#5ED4F4",
+  faJsSquare: "#EFD81D",
+  faGitAlt: "#EC4D28",
 };
 
 const About = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
+    const [iconSetIndex, setIconSetIndex] = useState(0);
 
     useEffect(() => {
+        const intervalId = setInterval(() => {
+            setIconSetIndex((current) => (current + 1) % iconSets.length);
+        }, 5000);
+
         setTimeout(() => {
             setLetterClass('text-animate-hover');
         }, 3000);
+        return () => clearInterval(intervalId);
     }, []);
 
     return (
@@ -41,12 +56,23 @@ const About = () => {
                 </div>
 
                 <div className="stage-cube-cont">
-                    <div className="cubespinner">
-                        {Object.entries(iconColors).map(([icon, color]) => (
-                            <div className={`face${icon}`} key={icon}>
-                                <FontAwesomeIcon icon={['fab', icon]} color={color} />
-                            </div>
-                        ))}
+                    <div className="face1">
+                        <FontAwesomeIcon icon={faAngular} color={iconColors[faAngular.iconName]} />
+                    </div>
+                    <div className="face2">
+                        <FontAwesomeIcon icon={faHtml5} color={iconColors[faHtml5.iconName]} />
+                    </div>
+                    <div className="face3">
+                        <FontAwesomeIcon icon={faCss3} color={iconColors[faCss3.iconName]} />
+                    </div>
+                    <div className="face4">
+                        <FontAwesomeIcon icon={faReact} color={iconColors[faReact.iconName]} />
+                    </div>
+                    <div className="face5">
+                        <FontAwesomeIcon icon={faJsSquare} color={iconColors[faJsSquare.iconName]} />
+                    </div>
+                    <div className="face6">
+                        <FontAwesomeIcon icon={faGitAlt} color={iconColors[faGitAlt.iconName]} />
                     </div>
                 </div>
             </div>
