69e8dc37ed66a0f43735cd4b8cad0e6a0a34ec9c
JohnM89 <mjammer2@gmail.com>
2024-03-18 21:44:43 -0400

    added animation for project section

--- src/components/Portfolio/portfolio.jsx
+++ src/components/Portfolio/portfolio.jsx
@@ -1,29 +1,29 @@
-import Project from '../Project/project';
-import projects from '../../data/projects';
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Project from '../Project/project';
+import projects from '../../data/projects';
+import AnimatedLetters from '../AnimatedLetters/animatedletters';
 import './portfolio.scss';
 
 gsap.registerPlugin(ScrollTrigger);
 
-
 const Portfolio = () => {
+ 
+  const [letterClass,] = useState('text-animate');
+
   useEffect(() => {
-    // padding at the start to allow scrolling to the first project
+
     gsap.set('.portfolio', { paddingTop: '100vh' });
     ScrollTrigger.refresh();
 
     projects.forEach((project, index) => {
       const projectClass = `.project-${index}`;
 
-      // entry and exit animations for each project
+      // Entry and exit animations for each project
       gsap.fromTo(
         projectClass,
-        {
-          autoAlpha: 0,
-          x: () => (index % 2 === 0 ? '-100%' : '100%'), // entry direction
-        },
+        { autoAlpha: 0, x: () => (index % 2 === 0 ? '-100%' : '100%') }, // entry direction
         {
           duration: 1,
           autoAlpha: 1,
@@ -34,17 +34,18 @@ const Portfolio = () => {
             start: "top center+=100", // start point for animation
             end: "bottom top-=100", // end point for animation
             toggleActions: "play none none reverse",
-            markers: false, 
+            markers: false,
             onLeave: () => gsap.to(projectClass, {
               duration: 1,
               autoAlpha: 0,
               x: () => (Math.random() < 0.5 ? '-100%' : '100%'),
               scale: 0.5
             }),
+            // entry animation for the project
             onEnterBack: () => gsap.fromTo(projectClass, {
               autoAlpha: 0,
               scale: 0.5,
-              x: () => (Math.random() < 0.5 ? '-100%' : '100%'), 
+              x: () => (Math.random() < 0.5 ? '-100%' : '100%'),
             }, {
               duration: 1,
               autoAlpha: 1,
@@ -59,6 +60,14 @@ const Portfolio = () => {
 
   return (
     <div className="portfolio">
+      <h1>
+        
+        <AnimatedLetters
+          letterClass={letterClass}
+          strArray={['↓','P', 'o', 'r', 't', 'f', 'o', 'l', 'i', 'o', '↓']}
+          idx={15}
+        />
+      </h1>
       {projects.map((project, index) => (
         <div key={project.id} className={`project project-${index}`}>
           {project.liveLink ? (
@@ -68,12 +77,12 @@ const Portfolio = () => {
           ) : (
             <img src={project.imageUrl} alt={project.title} />
           )}
+          {/* Passing project props to the Project component */}
           <Project {...project} />
         </div>
       ))}
     </div>
   );
 };
-
 
 export default Portfolio;
