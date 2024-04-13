68ebf58aab8c8e31e34f31a2b4a7f6317b428683
JohnM89 <mjammer2@gmail.com>
2024-04-12 23:17:11 -0400

    div styling w/react95

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,12 @@ import Portfolio from './components/Portfolio/portfolio';
 import Contact from './components/Contact/contact';
 import Resume from './components/Resume/resume';
 import ParallaxSection from './utils/parallaxsection';
+import { GroupBox, Button, ScrollView, Window, WindowContent, WindowHeader, styleReset } from 'react95';
+import styled from 'styled-components';
 
 
 
 // Import the necessary React95 components and styles
-import { MenuList, MenuListItem, Separator, styleReset } from 'react95';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
 /* Pick a theme of your choice */
@@ -41,6 +42,58 @@ const GlobalStyles = createGlobalStyle`
   }
 `;
 
+const Wrapper = styled.div`
+  padding: 5rem;
+  background: ${({ theme }) => theme.desktopBackground};
+  .window-title {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .close-icon {
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    margin-left: -1px;
+    margin-top: -1px;
+    transform: rotateZ(45deg);
+    position: relative;
+    &:before,
+    &:after {
+      content: '';
+      position: absolute;
+      background: ${({ theme }) => theme.materialText};
+    }
+    &:before {
+      height: 100%;
+      width: 3px;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+    &:after {
+      height: 3px;
+      width: 100%;
+      left: 0px;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+  }
+  .window {
+    width: 400px;
+    min-height: 200px;
+  }
+  .window:nth-child(2) {
+    margin: 2rem;
+  }
+  .footer {
+    display: block;
+    margin: 0.25rem;
+    height: 31px;
+    line-height: 31px;
+    padding-left: 0.25rem;
+  }
+`;
+
 function App() {
   return (
     <Router>
@@ -67,8 +120,27 @@ function App() {
             <section id="contact">
               <Contact />
             </section>
-            <ParallaxSection id="parallax-2" backgroundImage="/images/water.webp"
+
+
+              
+              
+            <Window>
+              
+            
+             
+              <WindowContent >
+                <ScrollView
+          style={{ background: 'teal' }}>
+                {/* <GroupBox variant='flat'> */}
+            <ParallaxSection id="parallax-2" backgroundImage="/images/setup.jpg"
             className='parallax-custom-2' />
+            {/* </GroupBox> */}
+            </ScrollView>
+            </WindowContent>
+            </Window>
+           
+
+
             <section id="resume">
               <Resume />
             </section>
--- src/components/Resume/resume.jsx
+++ src/components/Resume/resume.jsx
@@ -2,8 +2,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
-import { GroupBox,  MenuList, MenuListItem, ScrollView, Toolbar, Separator, Button, TextInput, Window, WindowContent, WindowHeader } from 'react95';
-import styled from 'styled-components';
+// import { GroupBox,  MenuList, MenuListItem, ScrollView, Toolbar, Separator, Button, TextInput, Window, WindowContent, WindowHeader } from 'react95';
+// import styled from 'styled-components';
 import '../Resume/resume.scss';
 
 const Resume = () => {
@@ -11,22 +11,38 @@ const Resume = () => {
         <div className="resume-container">
             
             <div className="image-placeholders">
+                <div className="social-links">
                 <img src="/images/logo1.png" alt="Logo 1" className="placeholder" />
-                <img src="/images/logo3.png" alt="Logo 2" className="placeholder" />
-                <img src="./images/logo2.png" alt="Logo 3" className="placeholder" />
-            </div>
-            <div className="social-links">
-                <a href="https://github.com/JohnM89" target="git" rel="noopener noreferrer">
+                                <a href="https://github.com/JohnM89" target="git" rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faGithub} /> GitHub
                     
                 </a>
-                <a href="https://www.linkedin.com/in/john-macneil-981316203/" target="link" rel="noopener noreferrer">
+                </div>
+                <div className="social-links2">
+                <img src="/images/logo3.png" alt="Logo 2" className="placeholder" />
+                                <a href="https://www.linkedin.com/in/john-macneil-981316203/" target="link" rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
                 </a>
-                <a href="/pdf/John MToronto 1 (1).pdf" target="pdf" rel="noopener noreferrer">
+                </div>
+                <div className="social-links3">
+                <img src="./images/logo2.png" alt="Logo 3" className="placeholder" />
+                                <a href="/pdf/John MToronto 1 (1).pdf" target="pdf" rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faFilePdf} /> Resume PDF
                 </a>
+                </div>
             </div>
+            
+                {/* <a href="https://github.com/JohnM89" target="git" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faGithub} /> GitHub
+                    
+                </a> */}
+                {/* <a href="https://www.linkedin.com/in/john-macneil-981316203/" target="link" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+                </a> */}
+                {/* <a href="/pdf/John MToronto 1 (1).pdf" target="pdf" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faFilePdf} /> Resume PDF
+                </a> */}
+            
         </div>
     );
 };
